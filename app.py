49428759b9f0284732c5b1f2f90233dff589372c
SlipRealm <sliprealmfounder@gmail.com>
2026-05-03 14:23:52 -0400

    Reposition logo and update title in app.py
    Moved logo image to the center column and updated title.

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 st.set_page_config(page_title="KGF Handicapper", page_icon="🐱", layout="wide")
 
-st.image("logo.jpg", width=400)
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("logo.jpg", width=400)
 
-st.title("🐱 Kitty Goldfinger (KGF) Handicapper")
+st.title("Kitty Goldfinger (KGF) Handicapper")
 st.subheader("Kitty Style Handicapping Logic | Honoring Kitty's Techniques")
 
 def kgf_score(horse_name, speed, stamina, odds, board_hit_rate=0.0):
