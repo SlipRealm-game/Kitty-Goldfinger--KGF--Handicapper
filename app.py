f9772bab4aaa62932dc25d9f62a814bb5b0f256a
SlipRealm <sliprealmfounder@gmail.com>
2026-05-10 16:20:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ try:
 except ImportError:
     BS4_AVAILABLE = False
 
-st.set_page_config(page_title="KGF Handicapper", page_icon="logo.jpg", layout="wide")
+st.set_page_config(page_title="KGF Handicapper", page_icon="logo2.jpg", layout="wide")
 
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.image("logo.jpg", width=400)
 
-st.title("🐱 Kitty Goldfinger (KGF) Handicapper")
+st.title("logo2.jpg", width=40 Kitty Goldfinger (KGF) Handicapper")
 st.subheader("Kitty Style Handicapping Logic | Kitty's Proven Techniques")
 
 def kgf_score(horse_name, speed, stamina, odds, board_hit_rate=0.0, pedigree_note=""):
